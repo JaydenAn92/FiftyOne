67afa1896bd2521be8f73153a062ea213efc0015
Jayden(안홍범) <anbum8463@gmail.com>
2023-02-20 16:13:52 +0900

    setting gnb

--- ffo/src/router/index.js
+++ ffo/src/router/index.js
@@ -8,13 +8,48 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/work',
+    name: 'work',
+    // route level code-splitting
+    // this generates a separate chunk (work.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "work", webpackPrefetch:true */ '../views/WorkView.vue'
+      )
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      import(
+        /* webpackChunkName: "about", webpackPrefetch:true */ '../views/AboutView.vue'
+      )
+  },
+  {
+    path: '/culture',
+    name: 'culture',
+    // route level code-splitting
+    // this generates a separate chunk (culture.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "culture", webpackPrefetch:true */ '../views/CultureView.vue'
+      )
+  },
+  {
+    path: '/recruit',
+    name: 'recruit',
+    // route level code-splitting
+    // this generates a separate chunk (recruit.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "recruit", webpackPrefetch:true */ '../views/RecruitView.vue'
+      )
   }
 ]
 
---
+++ ffo/src/views/CultureView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="culture">
+    <h1>This is an Culture page</h1>
+  </div>
+</template>
