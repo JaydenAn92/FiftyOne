971b3f92c353c5bc94c7dd6542a11d05c89a3ea7
sol <79960346+solpark16@users.noreply.github.com>
2023-02-21 09:36:33 +0900

    nav update

--- ffo/src/App.vue
+++ ffo/src/App.vue
@@ -1,16 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/work">Work</router-link> |
-    <router-link to="/about">About us</router-link> |
-    <router-link to="/about/who">Who we are</router-link> |
-    <router-link to="/about/contact">Contact us</router-link> |
-    <router-link to="/culture">Culture</router-link> |
-    <router-link to="/culture/hello">Hello! the fifty one</router-link> |
-    <router-link to="/culture/insight">Insight</router-link> |
-    <router-link to="/recruit">Recruit</router-link>
-  </nav>
-  <button @click="gnbBtnClick()">gnb</button>
+  <div class="navScreen">
+    <nav>
+      <router-link to="/" @click="gnbBtnClick()">Home</router-link>
+      <router-link to="/work" @click="gnbBtnClick()">Work</router-link>
+      <router-link to="/about" @click="gnbBtnClick()">About us</router-link>
+      <router-link to="/about/who" @click="gnbBtnClick()"
+        >Who we are</router-link
+      >
+      <router-link to="/about/contact" @click="gnbBtnClick()"
+        >Contact us</router-link
+      >
+      <router-link to="/culture" @click="gnbBtnClick()">Culture</router-link>
+      <router-link to="/culture/hello" @click="gnbBtnClick()"
+        >Hello! the fifty one</router-link
+      >
+      <router-link to="/culture/insight" @click="gnbBtnClick()"
+        >Insight</router-link
+      >
+      <router-link to="/recruit" @click="gnbBtnClick()">Recruit</router-link>
+    </nav>
+  </div>
+  <button class="navBtn" @click="gnbBtnClick()">gnb</button>
   <router-view />
 </template>
 
@@ -23,9 +33,22 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+.navScreen {
+  padding: 0;
+  background-color: #111111;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  opacity: 0.93;
   display: none;
+  align-items: center;
+  justify-content: center;
+}
+
+.navScreen.active {
+  display: flex;
 }
 
 nav a {
@@ -33,8 +56,18 @@ nav a {
   color: #2c3e50;
 }
 
+nav {
+  display: flex;
+  flex-direction: column;
+}
+
+.navBtn {
+  position: relative;
+  z-index: 100;
+}
+
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #ffffff;
 }
 </style>
 
@@ -45,13 +78,15 @@ export default {
   components: {},
   methods: {
     gnbBtnClick() {
-      const wef = document.querySelector('nav')
-      console.log(wef)
+      const navScreen = document.querySelector('.navScreen')
+      console.log(navScreen)
       if (num === '1') {
-        wef.style.display = 'block'
+        // navScreen.style.display = 'block'
+        navScreen.classList.add('active')
         num = '2'
       } else if (num === '2') {
-        wef.style.display = 'none'
+        // navScreen.style.display = 'none'
+        navScreen.classList.remove('active')
         num = '1'
       }
     }
--- ffo/src/views/WorkView.vue
+++ ffo/src/views/WorkView.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="work">
-    <Nav></Nav>
     <h1>This is an work page</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Nav from '@/views/NavView.vue'
 
 export default {
   name: 'WorkView',
-  components: {
-    Nav
-  }
+  components: {}
 }
 </script>
