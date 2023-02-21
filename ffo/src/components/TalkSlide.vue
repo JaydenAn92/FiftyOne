2cfe44c9d6179c4a9c2ed7f48bcac7f3c50900f3
choiBom <bchoi@the-51.com>
2023-02-21 14:40:52 +0900

    update

---
+++ ffo/src/components/TalkSlide.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="cultureInsight">
+    <swiper
+      :modules="modules"
+      :slides-per-view="4"
+      :space-between="50"
+      navigation
+      :pagination="{ clickable: true }"
+      :scrollbar="{ draggable: true }"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+    >
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide 4</swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TalkSlide'
+}
+</script>
--- ffo/src/views/CultureView.vue
+++ ffo/src/views/CultureView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="culture">
     <h1>This is an Culture page</h1>
+    <TalkSlide text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas laboriosam odio sapiente eveniet illo libero suscipit voluptatem at odit. Aperiam explicabo eaque cumque dolor quod? Pariatur numquam modi voluptates nihil?" title="Lorem" subtitle="Lorem ipsum" />
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import TalkSlide from '@/components/TalkSlide.vue'
+export default {
+  name: 'CultureView',
+  components: {
+    TalkSlide
+  }
+}
+</script>
