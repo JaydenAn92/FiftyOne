ce7277d7d81ebc8de9d476994c857c3fe54e5e8a
soyeon <soyean94@naver.com>
2023-02-21 17:33:15 +0900

    update

--- ffo/src/views/HomeView.vue
+++ ffo/src/views/HomeView.vue
@@ -2,16 +2,24 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <RecruitGride/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+// export default {
+//   name: 'HomeView',
+//   components: {
+//     HelloWorld
+//   }
+// }
+import RecruitGride from '@/components/RecruitGride.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    RecruitGride
   }
 }
 </script>
