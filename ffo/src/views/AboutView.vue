5fb07ae7619bd4833c49d0e20398cc9ce37ef57f
Jayden(안홍범) <anbum8463@gmail.com>
2023-02-21 17:01:54 +0900

    update About us

--- ffo/src/views/AboutView.vue
+++ ffo/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
+    <AboutUs :flag="flag" :title="title" :paragraph="paragraph" />
     <SwiperTemplate />
   </div>
 </template>
@@ -7,10 +8,20 @@
 <script>
 // import FullPageTop from '@/components/FullPageTop.vue'
 import SwiperTemplate from '@/components/SwiperTemplate.vue'
+import AboutUs from '@/components/AboutUs.vue'
 export default {
   components: {
     // FullPageTop,
-    SwiperTemplate
+    SwiperTemplate,
+    AboutUs
+  },
+  data() {
+    return {
+      flag: 'ABOUT US',
+      title: ['Awesome', 'Global Capability'],
+      paragraph:
+        '우리는 다양한 프로젝트 경험과 구성인력의 전문화로 조직간 Co-work을 통해 성공적인 프로젝트를 진행합니다.'
+    }
   }
 }
 </script>
