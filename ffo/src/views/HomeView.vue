b90dd09853bb52a18a639d946c09713b2a962724
Jayden(안홍범) <anbum8463@gmail.com>
2023-02-21 15:48:16 +0900

    update reset.css

--- ffo/src/views/HomeView.vue
+++ ffo/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -17,5 +17,5 @@ export default {
 </script>
 
 <style lang="scss">
-  @import "@/assets/scss/views/home";
+@import '@/assets/scss/views/home';
 </style>
