18fae56eda09043f1fc7af00d0425296b3b79b95
gosoohyeon <kosh1013@gmail.com>
2023-02-21 16:05:00 +0900

    update about>awards

---
+++ ffo/src/components/about/awards.vue
@@ -0,0 +1,89 @@
+<template>
+    <article class="awards-list">
+        <div class="awards-item" v-for="(item, index) in awardData" :key="index">
+            <div class="awards-item__inr">
+                <span class="awards-item__title">{{item.title}}</span>
+                <span class="awards-item__desc">{{item.desc}}</span>
+                <span class="awards-item__year">{{item.year}}</span>
+                <router-link :to="item.link" class="awards-item__link" v-show="item.link.length"><span>{{item.title}}이동</span></router-link>
+            </div>
+        </div>
+    </article>
+</template>
+
+<script>
+export default {
+  name: 'AboutAwards',
+  components: {},
+  props: {
+    awardData: Array
+  }
+}
+</script>
+
+<style lang="scss">
+.awards {
+  &-list {
+    padding: 30px;
+  }
+  &-item {
+    position: relative;
+
+    &:not(:last-child) {
+        border-bottom: 1px solid rgba(255,255,255,0.12);
+    }
+    &:after {
+        content:'';
+        display: block;
+        position: absolute;
+        bottom:0;
+        left:0;
+        z-index: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+        transform-origin: 50% 100%;
+        transform: perspective(1000px) rotateX(90deg);
+        transition: all .4s cubic-bezier(0.2,0,0.15,1);
+    }
+    &:hover:after {
+        background-color: #ff7061;
+        opacity: 1;
+        transform: perspective(1000px) rotateX(0);
+    }
+    &__inr {
+        display: flex;
+        position: relative;
+        z-index: 1;
+        padding: 22px;
+        font-size: 16px;
+        color: #fff;
+    }
+
+    &__title {
+        width: 25%;
+    }
+    &__desc {
+        flex:1;
+    }
+    &__year {
+        width: 25%;
+    }
+    &__link {
+        position: absolute;
+        top:0;
+        left:0;
+        width:100%;
+        height: 100%;
+
+        span {
+            overflow: hidden;
+            font-size: 0;
+            width: 0;
+            height: 0;
+            text-indent: -99px;
+        }
+    }
+  }
+}
+</style>
--- ffo/src/views/AboutView.vue
+++ ffo/src/views/AboutView.vue
@@ -1,5 +1,81 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <section class="award">
+      <h5>AWARDS</h5>
+      <div class="award-title-desc">
+        <p class="parallaxItem">We’ve received many</p>
+        <p class="parallaxItem">awards, and we’re not</p>
+        <p class="parallaxItem">very shy about it.</p>
+      </div>
+      <Awards :awardData="awardData" class="parallaxItem"/>
+    </section>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Awards from '@/components/about/awards.vue'
+import AwardData from '@/assets/data/about.json'
+export default {
+  name: 'AboutView',
+  components: {
+    Awards
+  },
+  data() {
+    return {
+      awardData: AwardData.awards,
+      currentScrollPos: 0
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.updateCurrentScrollPos, false)
+  },
+  methods: {
+    settingParallaxItem() {
+      const parallaxItemList = document.querySelectorAll('.parallaxItem')
+      parallaxItemList.forEach((el) => {
+        const pos = el.offsetTop
+        if (this.currentScrollPos > pos) el.classList.add('parallaxItem--active')
+        else el.classList.remove('parallaxItem--active')
+      })
+    },
+    updateCurrentScrollPos() {
+      this.currentScrollPos = window.scrollY + (window.innerHeight * 0.75) // window 4분의1 지점에서 show
+      this.settingParallaxItem()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .award {
+    padding-top: 114px;
+    background: #000;
+
+    h5 {
+      margin-bottom: 24px;
+      font-size: 16px;
+      font-weight: 700;
+      color: #fff;
+    }
+    &-title-desc {
+      margin-bottom: 104px;
+      p {
+        font-size: 47px;
+        color: #fff;
+      }
+    }
+  }
+  .parallaxItem {
+    position: relative;
+    top:30px;
+    opacity: 0;
+    transition: top .3s, opacity .3s;
+
+    &--active {
+      top:0;
+      opacity: 1;
+    }
+  }
+</style>
