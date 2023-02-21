6df2974c641dbf157a1122de5b41528a51131545
Jayden(안홍범) <anbum8463@gmail.com>
2023-02-21 11:45:19 +0900

    update meta tag

--- ffo/src/router/index.js
+++ ffo/src/router/index.js
@@ -5,11 +5,17 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    meta: {
+      title: 'Home - The Fiftyone - The 51 - digital marketing agency'
+    }
   },
   {
     path: '/work',
     name: 'work',
+    meta: {
+      title: 'Work - Slider Portfolio - The 51 - digital marketing agency'
+    },
     // route level code-splitting
     // this generates a separate chunk (work.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -21,6 +27,9 @@ const routes = [
   {
     path: '/about',
     name: 'about',
+    meta: {
+      title: 'About us - Slider Portfolio - The 51 - digital marketing agency'
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -32,6 +41,10 @@ const routes = [
   {
     path: '/culture',
     name: 'culture',
+    meta: {
+      title:
+        'Culture | Hello the fifty one - Slider Portfolio - The 51 - digital marketing agency'
+    },
     // route level code-splitting
     // this generates a separate chunk (culture.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -43,6 +56,9 @@ const routes = [
   {
     path: '/recruit',
     name: 'recruit',
+    meta: {
+      title: 'Recruit - The Fiftyone - The 51 - digital marketing agency'
+    },
     // route level code-splitting
     // this generates a separate chunk (recruit.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -56,6 +72,10 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`
 })
 
 export default router
