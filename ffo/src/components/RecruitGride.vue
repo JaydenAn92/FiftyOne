77010ddb0f82bda9cbf3a37c84fe576ccf8ca617
soyeon <soyean94@naver.com>
2023-02-21 14:54:19 +0900

    RecruitGride.vue

---
+++ ffo/src/components/RecruitGride.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="recruit">
+        <h3>점심부터 휴가까지, 회사 다닐 맛 나는</h3>
+        <p>THE51의 남다른 혜택</p>
+        <ul class="cont-area">
+            <li class="cont-text">
+                <span>01</span>
+                <p>
+                    매일 매일 걱정 없는
+                    <em>Happy Lunch</em>
+                </p>
+                <ul>
+                    <li>점심 식사용 법인카드 개인별 지급</li>
+                </ul>
+            </li>
+            <li>
+                <span>02</span>
+                <p>
+                    떠나고 싶을 때 떠나는
+                    <em>Vacation</em>
+                </p>
+                <ul>
+                    <li>연차는 기본, 일주일 여름휴가까지</li>
+                    <li>경조휴가, 출산휴가, 육아휴직도 눈치 NO!</li>
+                    <li>입사 3년, 6년에 한 번 2주 리프레시 휴가</li>
+                </ul>
+            </li>
+            <li>
+                <span>03</span>
+                <p>
+                    열심히 일한 당신께 드리는
+                    <em>Reward &amp; Gift</em>
+                </p>
+                <ul>
+                    <li>매년 우수부서, 우수직원 시상에 따라 상금 지급</li>
+                    <li>설날 &amp; 추석 명절에 백화점 상품권 지급</li>
+                    <li>선임급 이상 인재 추천 시 100만원 지급</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+  name: 'RecruitGride'
+}
+</script>
+<style>
+    .recruit {
+        background-color: #292929;
+        color: #fff;
+        padding: 30px 0;
+        text-align: left;
+    }
+    .recruit ul {
+        list-style: none;
+        display: flex;
+    }
+</style>
