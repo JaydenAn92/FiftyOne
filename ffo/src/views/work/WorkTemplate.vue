5def7bf5fcd8e18c8a4cdd9db4a3fc0b93f421a4
KimChaeYoung <rlacodud0313@naver.com>
2023-02-22 14:45:06 +0900

    WorkTemplate_update

---
+++ ffo/src/views/work/WorkTemplate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="work-template">
+    <!-- <WorkTemplate :data="$route.params" /> -->
+    {{ title }}
+    {{ $route.params.name }}
+  </div>
+</template>
+
+<script>
+// import WorkTemplate from '@/components/WorkTemplate.vue'
+// import ProductList from '@/assets/data/projectList.json'
+
+export default {
+  name: 'work-template',
+  components: {
+    // WorkTemplate
+  }
+}
+</script>
