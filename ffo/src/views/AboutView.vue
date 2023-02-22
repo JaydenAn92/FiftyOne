893bf7e9f56ec8e0a9ad9b48a80ab849d0c177e5
gosoohyeon <kosh1013@gmail.com>
2023-02-22 11:09:12 +0900

    about-us intro

--- ffo/src/views/AboutView.vue
+++ ffo/src/views/AboutView.vue
@@ -1,63 +1,68 @@
 <template>
   <div class="about">
     <section class="about">
-      <article class="about-main">
-        <div class="about-main__lt">
-          <h5>ABOUT US</h5>
+      <div class="container">
+        <article class="about-main">
+          <div class="about-main__lt">
+            <h5>ABOUT US</h5>
+            <div class="about__heading-subject">
+              <p class="parallaxItem">Awesome</p>
+              <p class="parallaxItem">Global Capability</p>
+            </div>
+            <div class="about__heading-desc">
+              우리는 다양한 프로젝트 경험과 구성인력의 전문화로 조직간 Co-work을
+              통해 <br />성공적인 프로젝트를 진행합니다.
+            </div>
+            <div class="about-sns-link">
+              <a
+                href="https://blog.naver.com/the_51"
+                target="_blank"
+                class="about-sns-link__item"
+              >
+                <font-awesome-icon icon="fa-brands fa-facebook-f" />
+              </a>
+              <a
+                href="https://www.youtube.com/channel/UCWD7HJBP5pbpu_NHeb6tBEw"
+                target="_blank"
+                class="about-sns-link__item"
+              >
+                <font-awesome-icon icon="fa-brands fa-youtube" />
+              </a>
+              <a
+                href="https://www.instagram.com/the_fiftyone/"
+                target="_blank"
+                class="about-sns-link__item"
+              >
+                <font-awesome-icon icon="fa-brands fa-instagram" />
+              </a>
+            </div>
+          </div>
+          <div class="about-main__rt">
+            <div class="about-main__img">
+              <img
+                src="http://www.the-51.com/wp-content/uploads/2019/06/about-submain-visual.jpg"
+                srcset="
+                  http://www.the-51.com/wp-content/uploads/2019/06/about-submain-visual.jpg         960w,
+                  http://www.the-51.com/wp-content/uploads/2019/06/about-submain-visual-600x621.jpg 600w,
+                  http://www.the-51.com/wp-content/uploads/2019/06/about-submain-visual-628x650.jpg 628w,
+                  http://www.the-51.com/wp-content/uploads/2019/06/about-submain-visual-768x795.jpg 768w
+                "
+                alt=""
+                />
+            </div>
+        </div>
+        </article>
+
+        <article class="about-awards">
+          <h5>AWARDS</h5>
           <div class="about__heading-subject">
-            <p class="parallaxItem">Awesome</p>
-            <p class="parallaxItem">Global Capability</p>
+            <p class="parallaxItem">We’ve received many</p>
+            <p class="parallaxItem">awards, and we’re not</p>
+            <p class="parallaxItem">very shy about it.</p>
           </div>
-          <div class="about__heading-desc">
-            우리는 다양한 프로젝트 경험과 구성인력의 전문화로 조직간 Co-work을
-            통해 <br />성공적인 프로젝트를 진행합니다.
-          </div>
-          <div class="about-sns-link">
-            <a
-              href="https://blog.naver.com/the_51"
-              target="_blank"
-              class="about-sns-link__item"
-            >
-              <font-awesome-icon icon="fa-brands fa-facebook-f" />
-            </a>
-            <a
-              href="https://www.youtube.com/channel/UCWD7HJBP5pbpu_NHeb6tBEw"
-              target="_blank"
-              class="about-sns-link__item"
-            >
-              <font-awesome-icon icon="fa-brands fa-youtube" />
-            </a>
-            <a
-              href="https://www.instagram.com/the_fiftyone/"
-              target="_blank"
-              class="about-sns-link__item"
-            >
-              <font-awesome-icon icon="fa-brands fa-instagram" />
-            </a>
-          </div>
-        </div>
-        <div class="about-main__rt">
-          <img
-            src="http://www.the-51.com/wp-content/uploads/2019/06/about-submain-visual.jpg"
-            srcset="
-              http://www.the-51.com/wp-content/uploads/2019/06/about-submain-visual.jpg         960w,
-              http://www.the-51.com/wp-content/uploads/2019/06/about-submain-visual-600x621.jpg 600w,
-              http://www.the-51.com/wp-content/uploads/2019/06/about-submain-visual-628x650.jpg 628w,
-              http://www.the-51.com/wp-content/uploads/2019/06/about-submain-visual-768x795.jpg 768w
-            "
-            alt=""
-          />
-        </div>
-      </article>
-      <article class="about-awards">
-        <h5>AWARDS</h5>
-        <div class="about__heading-subject">
-          <p class="parallaxItem">We’ve received many</p>
-          <p class="parallaxItem">awards, and we’re not</p>
-          <p class="parallaxItem">very shy about it.</p>
-        </div>
-        <Awards :awardData="awardData" class="parallaxItem" />
-      </article>
+          <Awards :awardData="awardData" class="parallaxItem" />
+        </article>
+      </div>
     </section>
   </div>
 </template>
