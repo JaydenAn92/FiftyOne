9333ba797ef9d91a765926b8f3e39e16530c11c7
gosoohyeon <kosh1013@gmail.com>
2023-02-23 11:31:20 +0900

    recruit > FullPageTop 적용

--- ffo/src/views/RecruitView.vue
+++ ffo/src/views/RecruitView.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="recruit">
+    <FullPageTop
+      title="<strong>PEOPLE <span>OF</span></strong><strong>THE <span>FIFTY ONE<em>.</em></span></strong>"
+      bgImg="http://www.the-51.com/wp-content/uploads/2022/05/img-bg-recruit.png"
+    />
     <div class="container">
       <RecruitBenefit />
     </div>
   </div>
 </template>
 <script>
+import FullPageTop from '@/components/FullPageTop.vue'
 import RecruitBenefit from '@/components/recruit/Benefit.vue'
 export default {
   name: 'RecruitView',
   components: {
+    FullPageTop,
     RecruitBenefit
   }
 }
