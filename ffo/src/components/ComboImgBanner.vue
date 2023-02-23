c39abad235f25a86bb23412e80aa346072ea504a
choiBom <bchoi@the-51.com>
2023-02-23 16:07:38 +0900

    update

--- ffo/src/components/ComboImgBanner.vue
+++ ffo/src/components/ComboImgBanner.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="combo-banner">
-      <p>{{ ComboImgBannerData }}</p>
         <div class="combo-banner__box" v-for="element in ComboImgBannerData" :key="{ element }">
-            <div :class="`${element.img ? combo-banner__item : `combo-banner__item--bg`}`" v-if="element.img || element.bg">
+            <div :class="combo-banner__item" v-if="element.img || element.bg">
                 <img class="combo-banner__img" :src="element.img" :alt="element.desc">
                 <span class="combo-banner__desc" v-if="element.desc">{{ element.desc }}</span>
             </div>
--- ffo/src/views/culture/InsightView.vue
+++ ffo/src/views/culture/InsightView.vue
@@ -38,7 +38,7 @@ export default {
   data() {
     return {
       cultureData: CultureData.workshop,
-      ComboImgBannerData: CultureData.comboBanner
+      ComboImgBannerData: CultureData.comboBanner.insight
     }
   }
 }
