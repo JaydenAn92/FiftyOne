9470f4bf12e05418a1bce30eef7bec9a22d721c4
gosoohyeon <kosh1013@gmail.com>
2023-02-23 17:37:12 +0900

    parallax animation js 전역으로 변경

--- ffo/src/main.js
+++ ffo/src/main.js
@@ -15,12 +15,16 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 
+/* import parallaxMotion js */
+import parallaxMotion from '@/assets/js/parallaxMotion'
+
 /* add icons to the library */
 library.add(fas, fab)
 
 createApp(App)
   .use(store)
   .use(router)
+  .use(parallaxMotion)
   .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app')
 createApp(AppFooter)
