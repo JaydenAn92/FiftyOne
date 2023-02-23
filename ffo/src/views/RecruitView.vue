b3efdaa18ee303e195ff74d5d4a4ddba1675e01a
soyeon <soyean94@naver.com>
2023-02-23 14:56:44 +0900

    update

--- ffo/src/views/RecruitView.vue
+++ ffo/src/views/RecruitView.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="recruit">
     <h1>This is an Recruit page</h1>
+    <RecruitGride/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import RecruitGride from '@/components/RecruitGride.vue'
+export default {
+  name: 'HomeView',
+  components: {
+    RecruitGride
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/views/home';
+</style>
