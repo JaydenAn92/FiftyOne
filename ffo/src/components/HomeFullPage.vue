354b80f8c1bd38641f07a718c0be99e085922e2d
Jayden(안홍범) <anbum8463@gmail.com>
2023-02-23 11:28:17 +0900

    Update HomeFullPage.vue

--- ffo/src/components/HomeFullPage.vue
+++ ffo/src/components/HomeFullPage.vue
@@ -69,14 +69,35 @@ export default {
       Lists[i].classList.add('current')
       clearInterval(this.runInterval)
       this.runInterval = setInterval(this.intervalSlide, 7000)
+      console.log(dots[i], i)
+      for (let j = 0; j < Lists.length; j++) {
+        if (j < i) {
+          // 클릭된 요소 보다 앞 순서
+          Lists[j].classList.remove('next')
+          Lists[j].classList.add('prev')
+
+          console.log('big')
+        } else if (j > i) {
+          // 클릭된 요소 보다 뒷 순서
+          Lists[j].classList.remove('prev')
+          Lists[j].classList.add('next')
+          console.log('small')
+        } else {
+          Lists[j].classList.remove('next')
+          Lists[j].classList.remove('prev')
+          console.log('same')
+        }
+      }
     },
     fninterval() {
       const slideLists = document.querySelectorAll('.project-slide')
       const navs = document.querySelectorAll('.dot')
-      for (const item in slideLists) {
-        if (item === '0') {
-          slideLists[item].classList.add('current')
-          navs[item].classList.add('current')
+      for (let i = 0; i < slideLists.length; i++) {
+        if (i === 0) {
+          slideLists[i].classList.add('current')
+          navs[i].classList.add('current')
+        } else {
+          slideLists[i].classList.add('next')
         }
       }
     },
@@ -90,27 +111,59 @@ export default {
       const slideIndex = getIndexItem('current', arrItem)
       slideLists.item(slideIndex).classList.remove('current')
       navs.item(slideIndex).classList.remove('current')
+      // there isn't a next element
       if (!slideLists.item(slideIndex).nextElementSibling) {
         slideLists.item(0).classList.add('current')
         navs.item(0).classList.add('current')
+        for (let i = 0; i < slideLists.length; i++) {
+          slideLists.item(i).classList.remove('prev')
+          if (i > 0) {
+            slideLists.item(i).classList.add('next')
+          }
+        }
       } else {
+        // there is a next element
+        slideLists.item(slideIndex).nextElementSibling.classList.remove('next')
         slideLists.item(slideIndex).nextElementSibling.classList.add('current')
         navs.item(slideIndex).nextElementSibling.classList.add('current')
+
+        for (let i = 0; i <= slideIndex; i++) {
+          slideLists.item(i).classList.remove('next')
+          slideLists.item(i).classList.add('prev')
+        }
       }
     },
     fnPrevBtn() {
       const navs = document.querySelectorAll('.dot')
       const currentNavs = document.querySelector('.dot.current')
-      const projectLists = document.querySelectorAll('.project-slide')
       const currentEl = document.querySelector('.project-slide.current')
+      const projectLists = document.querySelectorAll('.project-slide')
+      const arrItem = Array.from(projectLists)
+      const getIndexItem = (classToSearch, list) => {
+        return list.findIndex((elem) => elem.classList.contains(classToSearch))
+      }
+      const slideIndex = getIndexItem('current', arrItem)
       currentEl.classList.remove('current')
       currentNavs.classList.remove('current')
+      // there isn't a previous element
       if (!currentEl.previousElementSibling) {
+        projectLists.item(projectLists.length - 1).classList.remove('next')
         projectLists.item(projectLists.length - 1).classList.add('current')
         navs.item(navs.length - 1).classList.add('current')
+        for (let i = 0; i < projectLists.length; i++) {
+          projectLists.item(i).classList.remove('next')
+          if (i < projectLists.length - 1) {
+            projectLists.item(i).classList.add('prev')
+          }
+        }
       } else {
+        // there is a previous element
+        currentEl.previousElementSibling.classList.remove('prev')
         currentEl.previousElementSibling.classList.add('current')
         currentNavs.previousElementSibling.classList.add('current')
+        for (let i = slideIndex; i < projectLists.length; i++) {
+          projectLists.item(i).classList.add('next')
+        }
       }
       clearInterval(this.runInterval)
       this.runInterval = setInterval(this.intervalSlide, 7000)
@@ -120,14 +173,32 @@ export default {
       const currentNavs = document.querySelector('.dot.current')
       const projectLists = document.querySelectorAll('.project-slide')
       const currentEl = document.querySelector('.project-slide.current')
+      const arrItem = Array.from(projectLists)
+      const getIndexItem = (classToSearch, list) => {
+        return list.findIndex((elem) => elem.classList.contains(classToSearch))
+      }
+      const slideIndex = getIndexItem('current', arrItem)
       currentEl.classList.remove('current')
       currentNavs.classList.remove('current')
+      // there isn't a next element
       if (!currentEl.nextElementSibling) {
         projectLists.item(0).classList.add('current')
         navs.item(0).classList.add('current')
+        for (let i = 0; i < projectLists.length; i++) {
+          projectLists.item(i).classList.remove('prev')
+          if (i > 0) {
+            projectLists.item(i).classList.add('next')
+          }
+        }
       } else {
+        // there is a next element
+        currentEl.nextElementSibling.classList.remove('next')
         currentEl.nextElementSibling.classList.add('current')
         currentNavs.nextElementSibling.classList.add('current')
+        for (let i = 0; i <= slideIndex; i++) {
+          projectLists.item(i).classList.remove('next')
+          projectLists.item(i).classList.add('prev')
+        }
       }
       clearInterval(this.runInterval)
       this.runInterval = setInterval(this.intervalSlide, 7000)
