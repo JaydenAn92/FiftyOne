0a1865a8d53a6a517727c45bccda783f62fba465
choiBom <bchoi@the-51.com>
2023-02-27 10:32:30 +0900

    update WideBanner

--- ffo/src/components/ComboImgBanner.vue
+++ ffo/src/components/ComboImgBanner.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="combo-banner">
-        <div :class=" element.img ? 'combo-banner__item' : 'combo-banner__item--bg'" v-for="element in ComboImgBannerData" :key="{ element }" :style="{ background: `'${element.bg}'` }">
+        <div :class=" element.img ? 'combo-banner__item' : 'combo-banner__item combo-banner__item--bg'" v-for="element in ComboImgBannerData" :key="{ element }" :style="{ backgroundColor: element.bgColor ? `${element.bgColor}` : '' }">
             <img class="combo-banner__img" :src="element.img" alt="" v-if="element.img">
             <span class="combo-banner__title" v-if="element.title">{{ element.title }}</span>
         </div>
@@ -17,27 +17,30 @@ export default {
 <style lang="scss">
 .combo-banner {
   position:relative;
-  @media only screen and (min-width: 690px) {
+  @media only screen and (min-width: 1000px) {
     display: grid;
     grid-template-columns:repeat(4, 1fr);
-    grid-template-rows:repeat(2, 1fr)
+    grid-template-rows:repeat(2, 1fr);
+    gap: 0;
   }
   &__item {
     position: relative;
     &:last-child {
-      @media only screen and (min-width: 690px) {
+      @media only screen and (min-width: 1000px) {
         grid-row: 1 / span 2;
         grid-column: 3 / span 4;
+        gap:0;
       }
     }
     img {
       width:100%;
+      height:100%;
     }
   }
   &__title {
     position:absolute;
-    top:15px;
-    left:15px;
+    top:23px;
+    left:23px;
     color:#fff;
     font-size: 22px;
     line-height: 26px;
--- ffo/src/components/WideImgBanner.vue
+++ ffo/src/components/WideImgBanner.vue
@@ -1,8 +1,11 @@
 <template>
-<div class="wide-banner">
-  <div class="wide-banner__text-box">
-    <h2 class="wide-banner__text-box__title" v-if="ImgBannerData.title" v-html="ImgBannerData.title" />
-    <p class="wide-banner__text-box__desc" v-if="ImgBannerData.desc" v-html="ImgBannerData.desc" />
+<div class="wide-banner" :style="{ backgroundImage: ImgBannerData.bgImg ? `url(${ImgBannerData.bgImg})` : '' }">
+  <div class="wide-banner__container">
+    <h2 class="wide-banner__title" v-if="ImgBannerData.title" v-html="ImgBannerData.title" />
+    <p class="wide-banner__desc" v-if="ImgBannerData.desc" v-html="ImgBannerData.desc" />
+    <a :href="ImgBannerData.button.url" v-if="ImgBannerData.button" target="">
+      <span v-html="ImgBannerData.button.desc"></span>
+    </a>
   </div>
 </div>
 </template>
@@ -12,21 +15,70 @@ export default {
   props: {
     ImgBannerData: Array
   }
-  // computed: {
-  //   bgImgData() {
-  //     return {
-  //       "bannerBg": "#ddfddfd"
-  //     }
-  //   }
-  // }
 }
 </script>
 <style lang="scss">
 .wide-banner {
-  // background:url(var(bannerBg))
-  &__text-box {
-    &__title {}
-    &__desc {}
+  display: flex;
+  justify-content: center;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  &:before {
+      display: inline-block;
+      content:"";
+      clear:both;
+      max-width: 670.42px;
+      width: 48.9361%;
+      @media only screen and (max-width: 1000px) {
+        display: none;
+      }
+    }
+  &__container {
+    padding:calc(100vw * 0.10) 0;
+    max-width: 880px;
+    width: 100%;
+    color:#fff;
+    text-align: left;
+    @media only screen and (max-width: 690px) {
+      margin:0 auto;
+      max-width: 320px;
+    }
+    @media only screen and (min-width: 1000px) {
+      margin-left: 2.1%;
+      max-width: 670.42px;
+      width:48.9361%;
+    }
   }
+  &__title {
+    margin-bottom: 24px;
+    font-family: 'Chakra Petch', 'Noto Sans KR', sans-serif !important;
+    text-transform: capitalize;
+    letter-spacing: 0;
+    font-size: 56px;
+    line-height: 66px;
+    font-weight: 700;
+    -webkit-font-smoothing: antialiased;
+    @media only screen and (max-width: 690px) {
+      font-size: 42px;
+      line-height: 51.25px;
+    }
+    @media only screen and (min-width: 690px) {
+      font-size: 44.8px;
+      line-height: 54.2px;
+    }
+    @media only screen and (min-width: 1000px) {
+      font-size: 47.5px;
+      line-height: 50.15px;
+    }
+  }
+  &__desc {
+    color:rgba(255,255,255,.9);
+    font-family: 'Noto Sans KR', sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 30px;
+  }
+
 }
 </style>
