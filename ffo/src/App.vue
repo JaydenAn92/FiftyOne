696be57e456a60080f61a7d3594f91dfa092cf71
Jayden(안홍범) <anbum8463@gmail.com>
2023-02-28 15:33:30 +0900

    fixed layout components

--- ffo/src/App.vue
+++ ffo/src/App.vue
@@ -1,84 +1,6 @@
 <template>
   <!-- <PageTransition /> -->
-  <div class="navScreen">
-    <nav>
-      <ul class="navList">
-        <li>
-          <router-link to="/" @click="gnbBtnClick()">Home</router-link>
-        </li>
-        <li>
-          <router-link to="/work" @click="gnbBtnClick()">Work</router-link>
-        </li>
-        <li>
-          <router-link to="/about" @click="gnbBtnClick()">About us</router-link>
-          <div class="subMenu about">
-            <router-link to="/about/who" @click="gnbBtnClick()"
-              >Who we are</router-link
-            >
-            <router-link to="/about/contact" @click="gnbBtnClick()"
-              >Contact us</router-link
-            >
-          </div>
-        </li>
-        <li>
-          <router-link to="/culture" @click="gnbBtnClick()"
-            >Culture</router-link
-          >
-          <div class="subMenu culture">
-            <router-link to="/culture" @click="gnbBtnClick()"
-              >Hello! the fifty one</router-link
-            >
-            <router-link to="/culture/insight" @click="gnbBtnClick()"
-              >Insight</router-link
-            >
-          </div>
-        </li>
-        <li>
-          <router-link to="/recruit" @click="gnbBtnClick()"
-            >Recruit</router-link
-          >
-        </li>
-      </ul>
-    </nav>
-    <ul class="snsList">
-      <li class="facebook">
-        <a target="_blank" href="https://blog.naver.com/the_51">
-          <font-awesome-icon icon="fa-brands fa-facebook-f" />
-        </a>
-      </li>
-      <li class="youtube">
-        <a
-          target="_blank"
-          href="https://www.youtube.com/channel/UCWD7HJBP5pbpu_NHeb6tBEw"
-        >
-          <font-awesome-icon icon="fa-brands fa-youtube" />
-        </a>
-      </li>
-      <li class="instagram">
-        <a target="_blank" href="https://www.instagram.com/the_fiftyone/">
-          <font-awesome-icon icon="fa-brands fa-instagram" />
-        </a>
-      </li>
-    </ul>
-    <p class="bottom-text">© 2019 the Fiftyone Corp.</p>
-  </div>
-  <div class="header">
-    <router-link to="/" class="logo">
-      <img
-        class="default-logo"
-        src="http://www.the-51.com/wp-content/uploads/2019/06/header-logo.png"
-      />
-      <img
-        class="dark-logo"
-        src="http://www.the-51.com/wp-content/uploads/2019/06/header-logo_B.png"
-      />
-    </router-link>
-    <a class="navBtn" @click="gnbBtnClick()">
-      <span>
-        <i class="lines"></i>
-      </span>
-    </a>
-  </div>
+  <HeaderLayout />
   <div class="blurred">
     <!-- <router-view /> -->
     <router-view v-slot="{ Component, route }">
@@ -87,6 +9,7 @@
       </transition>
     </router-view>
   </div>
+  <FooterCom v-show="this.isMain" />
   <TopBtn />
 </template>
 
@@ -115,79 +38,31 @@
 <script>
 // import func from 'vue-editor-bridge'
 // import PageTransition from '@/components/PageTransition.vue'
+import HeaderLayout from '@/components/layouts/HeaderLayout.vue'
+import FooterCom from '@/components/layouts/FooterCom.vue'
 import TopBtn from '@/components/TopBtn.vue'
 
-let num = '1'
 export default {
   name: 'App',
+  data() {
+    return {
+      isMain: null
+    }
+  },
   components: {
     // PageTransition,
+    HeaderLayout,
+    FooterCom,
     TopBtn
   },
-  mounted() {
-    document.addEventListener('scroll', this.scrollEvents)
-  },
-  methods: {
-    gnbBtnClick() {
-      const blurred = document.querySelector('.blurred')
-      const navScreen = document.querySelector('.navScreen')
-      const nav = document.querySelector('nav')
-      const navBtn = document.querySelector('.navBtn')
-      const snsList = document.querySelector('.snsList')
-      const bottomText = document.querySelector('.bottom-text')
-      const app = document.querySelector('#app')
-      const logo = document.querySelector('.logo')
-      if (num === '1') {
-        navScreen.onwheel = function (e) {
-          e.preventDefault()
-          e.stopPropagation()
-        }
-        blurred.style.filter = 'blur(5px)'
-        // app.style.marginBottom = '0'
-        navBtn.classList.remove('dark')
-        logo.classList.remove('dark')
-        setTimeout(function () {
-          navScreen.classList.add('active')
-          nav.classList.add('active')
-          navBtn.classList.add('active')
-        }, 500)
-        setTimeout(function () {
-          snsList.classList.add('active')
-          bottomText.classList.add('active')
-        }, 1000)
-        num = '2'
-      } else if (num === '2') {
-        app.classList.remove('active')
-        // app.style.marginBottom = '412px'
-        blurred.style.filter = 'none'
-        navBtn.classList.remove('active')
-        snsList.classList.remove('active')
-        bottomText.classList.remove('active')
-        num = '1'
-        navScreen.classList.add('close')
-        setTimeout(function () {
-          navScreen.classList.remove('close')
-          navScreen.classList.remove('active')
-          nav.classList.remove('active')
-        }, 500)
+  watch: {
+    $route(to, from) {
+      if (to.name === 'home') {
+        this.isMain = false
+      } else {
+        this.isMain = true
       }
-    },
-    scrollEvents() {
-      const documentTop = document.documentElement.scrollTop
-      const whiteSec = document.querySelector('.white-section')
-      const logo = document.querySelector('.logo')
-      const navBtn = document.querySelector('.navBtn')
-      if (whiteSec) {
-        const whiteTop =
-          window.pageYOffset + whiteSec.getBoundingClientRect().top
-        if (documentTop > whiteTop - 30 && num === '1') {
-          navBtn.classList.add('dark')
-          logo.classList.add('dark')
-        } else {
-          navBtn.classList.remove('dark')
-          logo.classList.remove('dark')
-        }
-      }
+      console.log(to.name)
     }
   }
 }
