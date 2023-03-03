37e08cfda59daecd32750d8855598ecc56a08fd4
yhjang90 <yhjang@the-51.com>
2023-03-03 17:45:36 +0900

    insight hidden page

---
+++ ffo/src/components/InsightHiddenTemplate.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="news-detail-container">
+    <FullPageTop
+      :title="data.detail.newsTitle"
+      :subTitle="data.subTitle"
+      :bgImg="data.bg"
+      type="scroll"
+    />
+    <div class="news-detail-warp">
+      <div class="news-detail">
+        <div class="news-contents">
+          <h4 v-if="data.detail.newsTitle">{{ data.detail.newsTitle }}</h4>
+          <div class="logo-title-warp">
+            <div class="logo-title-img" v-if="data.detail.logoTitle.logoImg">
+              <img :data-src="data.detail.logoTitle.logoImg" />
+            </div>
+            <div class="logo-title-text" v-if="data.detail.logoTitle.logoText">
+              <img :data-src="data.detail.logoTitle.logoText" />
+            </div>
+          </div>
+          <div class="news-info">
+            <div v-for="newsDesc in data.detail.newsDesc" :key="{ newsDesc }">
+              <strong class="par-title" v-if="newsDesc.parTitle">{{
+                newsDesc.parTitle
+              }}</strong>
+              <p class="par-desc" v-if="newsDesc.parDesc">
+                {{ newsDesc.parDesc }}
+              </p>
+              <p class="par-info" v-if="newsDesc.parInfo">
+                {{ newsDesc.parInfo }}
+              </p>
+              <img
+                class="par-img"
+                v-if="newsDesc.parImg"
+                :src="newsDesc.parImg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <PaginationTemplate
+      :prevText="projectPrevId ? 'Previous Project' : ''"
+      :nextText="projectNextId ? 'Next Project' : ''"
+      prevPathName="insight-template"
+      nextPathName="insight-template"
+      :projectPrevId="projectPrevId ? projectPrevId : ''"
+      :projectNextId="projectNextId ? projectNextId : ''"
+      :projectPrevImg="projectPrev ? projectPrev.thumbnail : ''"
+      :projectNextImg="projectNext ? projectNext.thumbnail : ''"
+      :projectPrevTitle="projectPrev ? projectPrev.title : ''"
+      :projectNextTitle="projectNext ? projectNext.title : ''"
+      :arrow="true"
+    />
+  </div>
+</template>
+<script>
+import FullPageTop from '@/components/FullPageTop.vue'
+import PaginationTemplate from '@/components/PaginationTemplate.vue'
+
+export default {
+  components: {
+    FullPageTop,
+    PaginationTemplate
+  },
+  props: {
+    data: [Object, Function],
+    projectPrev: [Object, Function],
+    projectNext: [Object, Function],
+    projectPrevId: [String, Function],
+    projectNextId: [String, Function]
+  }
+}
+</script>
+<style lang="scss">
+.news-detail-warp {
+  position: relative;
+  z-index: 10;
+  font-family: 'Chakra Petch', 'Roboto', 'Noto Sans KR', sans-serif;
+  padding-top: calc(100vw * 0.1);
+  padding-bottom: calc(100vw * 0.1);
+  background-color: white;
+  .news-detail {
+    text-align: left;
+    max-width: 1550px;
+    margin: 0 auto;
+    padding: 0 90px;
+    .news-contents {
+      max-width: 1000px;
+      margin: 0 auto;
+      font-size: 14px;
+      color: #333;
+      line-height: 1.8;
+      .news-info > div {
+        padding-bottom: 27px;
+        display: inline-block;
+        &:last-child {
+          padding-bottom: 0;
+        }
+      }
+      h4 {
+        font-size: 20px;
+        line-height: 34px;
+        margin-bottom: 4%;
+        margin-top: 10px;
+        font-weight: 600;
+        @media only screen and (max-width: 690px) {
+          font-size: 18px;
+          line-height: 30.6px;
+        }
+      }
+      .par-img {
+        width: 100%;
+      }
+      .par-title {
+        font-weight: 600;
+      }
+    }
+    @media only screen and (max-width: 1000px) {
+      max-width: 600px;
+      padding: 0;
+    }
+    @media only screen and (max-width: 690px) {
+      max-width: 420px;
+    }
+    @media only screen and (max-width: 480px) {
+      max-width: 320px;
+    }
+  }
+}
+</style>
--- ffo/src/views/InsightHiddenView.vue
+++ ffo/src/views/InsightHiddenView.vue
@@ -7,7 +7,7 @@
     />
     <div class="news-list">
       <router-link
-        :to="{ name: 'insight-hidden', params: { id: insight.id } }"
+        :to="{ name: 'insight-template', params: { id: insight.id } }"
         v-for="insight in insightList"
         :key="{ insight }"
         :class="`thumb-warp ${insight.size} ${
@@ -35,34 +35,15 @@
         </div>
       </router-link>
     </div>
-    <!-- <CultureIntro
-      :title="['For A Better,', 'Company']"
-      content="3월 10일~11일 이틀간 가평의 교원비전센터에서 더피프티원의 관리자들의 역량 강화를 위한 워크샵이 진행되었습니다.
-이번 워크샵은 더피프티원의 중심축 역할을 해주시는 관리자들을 대상으로보다 발전적인 회사의 미래를 구상하고 관리자로서의 역할을 다시 한번 생각해보는 워크샵이었습니다. 지금부터 그 현장을 소개합니다."
-      :imgSrc="[
-        'http://www.the-51.com/wp-content/uploads/2019/06/culture-insight-cascading03.jpg',
-        'http://www.the-51.com/wp-content/uploads/2019/06/culture-insight-cascading02.jpg',
-        'http://www.the-51.com/wp-content/uploads/2019/06/culture-insight-cascading01.jpg'
-      ]"
-    />
-    <ComboImgBanner :ComboImgBannerData="ComboImgBannerData" />
-    <TalkSlide :cultureData="cultureData" /> -->
   </div>
 </template>
 
 <script>
 import FullPageTop from '@/components/FullPageTop.vue'
 import insightList from '@/assets/data/insightList.json'
-// import CultureIntro from '@/components/CultureIntro.vue'
-// import ComboImgBanner from '@/components/ComboImgBanner.vue'
-// import TalkSlide from '@/components/TalkSlide.vue'
-// import CultureData from '@/assets/data/culture.json'
 export default {
   components: {
     FullPageTop
-    // CultureIntro,
-    // ComboImgBanner,
-    // TalkSlide
   },
   data() {
     return {
