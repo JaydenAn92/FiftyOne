1caed719340c24083903931f7b0ceb5332ca9c7f
gosoohyeon <kosh1013@gmail.com>
2023-03-06 16:51:21 +0900

    update about-us

--- ffo/src/components/about/ContentLink.vue
+++ ffo/src/components/about/ContentLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <article :class="`about-content-link ${this.class}`">
+  <article :class="`about-content-link parallaxItem ${this.class}`">
     <h2 class="about-content-link__h2" v-html="title"></h2>
     <div class="about-content-link__desc" v-html="description"></div>
     <router-link :to="link" class="about-content-link__btn">
